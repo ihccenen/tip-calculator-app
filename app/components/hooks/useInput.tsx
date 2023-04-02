8d4f9234c498d8743e6154778f52fceec6609e34
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-04-02 13:45:32 -0300

    Added reset and changed regex

--- app/components/hooks/useInput.tsx
+++ app/components/hooks/useInput.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 
-export default function useInput(initialValue: string) {
+export default function useInput(initialValue: string, pattern?: RegExp) {
   const [value, setValue] = useState(initialValue);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const regex = /^[0-9]*\.?[0-9]*$/;
-
+    const regex = pattern || /^(?!0{2,})[0-9]*\.?[0-9]*$/;
     if (e.currentTarget.type === 'radio' || regex.test(e.currentTarget.value)) {
       setValue(e.currentTarget.value);
     }
   };
 
+  const reset = () => {
+    setValue(initialValue);
+  };
+
   return {
     value,
     handleChange,
+    reset,
   };
 }
